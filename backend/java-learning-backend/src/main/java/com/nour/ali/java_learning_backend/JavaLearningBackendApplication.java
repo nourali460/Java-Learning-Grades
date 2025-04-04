1324389ec98e43aadeb0bf3a472d651c3c8b1ea8
nourali460 <nour.h.ali460@gmail.com>
2025-04-03 22:26:47 -0500

    reset project to now use spring boot and postgres sql

---
+++ backend/java-learning-backend/src/main/java/com/nour/ali/java_learning_backend/JavaLearningBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nour.ali.java_learning_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLearningBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLearningBackendApplication.class, args);
+	}
+
+}
---
+++ backend/java-learning-backend/src/test/java/com/nour/ali/java_learning_backend/JavaLearningBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nour.ali.java_learning_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLearningBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
